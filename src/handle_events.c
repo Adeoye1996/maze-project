104885a3094f8d6fd7fa0c819dfc50b1c786588b
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-07-06 09:09:05 -0700

    handle_events.c initialize_instance.c

--- src/handle_events.c
+++ src/handle_events.c
@@ -11,33 +11,33 @@ void handle_events(keys *key_state) {
                 case SDLK_ESCAPE:
                     key_state->esc = true;
                     break;
-                case SDLK_UP:
-                    key_state->up = true;
+                case SDLK_w:
+                    key_state->w = true;
                     break;
-                case SDLK_DOWN:
-                    key_state->down = true;
+                case SDLK_s:
+                    key_state->s = true;
                     break;
-                case SDLK_LEFT:
-                    key_state->left = true;
+                case SDLK_a:
+                    key_state->a = true;
                     break;
-                case SDLK_RIGHT:
-                    key_state->right = true;
+                case SDLK_d:
+                    key_state->d = true;
                     break;
             }
         }
         if (event.type == SDL_KEYUP) {
             switch (event.key.keysym.sym) {
-                case SDLK_UP:
-                    key_state->up = false;
+                case SDLK_w:
+                    key_state->w = false;
                     break;
-                case SDLK_DOWN:
-                    key_state->down = false;
+                case SDLK_s:
+                    key_state->s = false;
                     break;
-                case SDLK_LEFT:
-                    key_state->left = false;
+                case SDLK_a:
+                    key_state->a = false;
                     break;
-                case SDLK_RIGHT:
-                    key_state->right = false;
+                case SDLK_d:
+                    key_state->d = false;
                     break;
             }
         }
